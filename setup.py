be2ea9485e2ea48f23cb22d94d37ab4b0c95042a
sararibeiro23 <157724117+sararibeiro23@users.noreply.github.com>
2024-01-25 15:46:03 +0000

    final change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='reverse_geocode',
-    version='1.5.1',
+    version='1.5+sara',
     packages=['reverse_geocode'],
     package_dir={'reverse_geocode': '.'},  # look for package contents in current directory
     package_data={'reverse_geocode': ['geocode.csv', 'countries.csv']},
